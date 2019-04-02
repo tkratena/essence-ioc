8202b0cb47ee797e817482aa21b79d6f8c3b2d00
Tomáš Kratěna <36986324+tkratena@users.noreply.github.com>
2019-04-03 00:58:46 +0200

    Documentation of Result structures updated

--- EssenceIoc/Essence.Framework/Model/Result.cs
+++ EssenceIoc/Essence.Framework/Model/Result.cs
@@ -8,7 +8,7 @@ namespace Essence.Framework.Model
     /// Structure to be used as a return type. The result can be a success or a failure.<para/>
     /// To use it, just define <see cref="Result"/> as return type of a method. Then to create the result, use
     /// <see cref="Result.Success"/> or <see cref="Result.Failure"/>. These will be implicitly converted to
-    /// <see cref="Result"/>.<para/>
+    /// <see cref="Result"/>. Or you can make use of implicit conversion from <see cref="bool"/>.<para/>
     /// To consume the result, use
     /// <see cref="Result.Case(Action,Action)"/> or <see cref="Result.Case{TResult}(Func{TResult},Func{TResult})"/>.
     /// <para/>
@@ -88,14 +88,90 @@ namespace Essence.Framework.Model
     }
 
     /// <summary>
+    /// Structure to be used as a return type. The result can be a success and contain a value or it can be a failure.
+    /// <para/>
+    /// To use it, just define <see cref="Result{TValue}"/> as return type of a method. Then, values can be directly
+    /// returned as success thanks to implicit conversion. Use <see cref="Result.Failure"/> method of
+    /// <see cref="Result"/> to create a failure.<para/>
+    /// Then, use methods of
+    /// If you want to be specific, you can use <see cref="Result.Success{TValue}"/> method of <see cref="Result"/>.
+    /// <see cref="Result.Failure{TError}"/>.<para/>
+    /// To consume the result, use
+    /// <see cref="Result{TValue}.Case(Action{TValue},Action)"/> or
+    /// <see cref="Result{TValue}.Case{TResult}(Func{TValue,TResult},Func{TResult})"/>.<para/>
+    /// </summary>
+    public struct Result<TValue>
+    {
+        private readonly TValue _value;
+        private readonly bool _isSuccess;
+
+        public static implicit operator Result<TValue>(TValue value)
+        {
+            return new Result<TValue>(value);
+        }
+        
+        public static implicit operator Result<TValue>(Success<TValue> success)
+        {
+            return new Result<TValue>(success.Value);
+        }
+        
+        public static implicit operator Result<TValue>(Failure failure)
+        {
+            return new Result<TValue>();
+        }
+
+        private Result(TValue value)
+        {
+            _value = value;
+            _isSuccess = true;
+        }
+
+        public void Case(Action<TValue> success, Action failure)
+        {
+            if (_isSuccess)
+            {
+                success(_value);
+            }
+            else
+            {
+                failure();
+            }
+        }
+
+        [Pure]
+        public TResult Case<TResult>(Func<TValue, TResult> success, Func<TResult> failure)
+        {
+            return _isSuccess ? success(_value) : failure();
+        }
+
+        [Pure]
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (EqualityComparer<TValue>.Default.GetHashCode(_value) * 397) ^ _isSuccess.GetHashCode();
+            }
+        }
+
+        [Pure]
+        public override string ToString()
+        {
+            return _isSuccess ? $"Success:{_value}" : "Failure";
+        }
+    }
+
+    /// <summary>
     /// Structure to be used as a return type. The result can be a success and contain a value or it can be a failure
     /// and contain an error.<para/>
     /// To use it, just define <see cref="Result{TValue,TError}"/> as return type of a method. Then, values and errors
     /// can be directly returned. Whether the result is a success or a failure will be inferred thanks to implicit
     /// conversion.<para/>
+    /// If you want to be specific or if your value and error types are the same, you can use methods of
+    /// <see cref="Result"/> to create the result; <see cref="Result.Success{TValue}"/> or
+    /// <see cref="Result.Failure{TError}"/>.<para/>
     /// To consume the result, use
     /// <see cref="Result{TValue,TError}.Case(Action{TValue},Action{TError})"/> or
-    /// <see cref="Result{TValue,TError}.Case{T}(Func{TValue,T},Func{TError,T})"/>.
+    /// <see cref="Result{TValue,TError}.Case{TResult}(Func{TValue,TResult},Func{TError,TResult})"/>.
     /// </summary>
     public struct Result<TValue, TError>
     {
@@ -174,76 +250,6 @@ namespace Essence.Framework.Model
         public override string ToString()
         {
             return _isSuccess ? $"Success:{_value}" : $"Failure:{_error}";
-        }
-    }
-
-    /// <summary>
-    /// Structure to be used as a return type. The result can be a success and contain a value or it can be a failure.
-    /// <para/>
-    /// To use it, just define <see cref="Result{TValue}"/> as return type of a method. Then, use methods of
-    /// <see cref="Result"/> to create the result; <see cref="Result.Success{TValue}"/> or <see cref="Result.Failure"/>.
-    /// These will be implicitly converted to <see cref="Result{T}"/>.<para/>
-    /// To consume the result, use
-    /// <see cref="Result{TValue}.Case(Action{TValue},Action)"/> or
-    /// <see cref="Result{TValue}.Case{TResult}(Func{TValue,TResult},Func{TResult})"/>.<para/>
-    /// </summary>
-    public struct Result<TValue>
-    {
-        private readonly TValue _value;
-        private readonly bool _isSuccess;
-
-        public static implicit operator Result<TValue>(TValue value)
-        {
-            return new Result<TValue>(value);
-        }
-        
-        public static implicit operator Result<TValue>(Success<TValue> success)
-        {
-            return new Result<TValue>(success.Value);
-        }
-        
-        public static implicit operator Result<TValue>(Failure failure)
-        {
-            return new Result<TValue>();
-        }
-
-        private Result(TValue value)
-        {
-            _value = value;
-            _isSuccess = true;
-        }
-
-        public void Case(Action<TValue> success, Action failure)
-        {
-            if (_isSuccess)
-            {
-                success(_value);
-            }
-            else
-            {
-                failure();
-            }
-        }
-
-        [Pure]
-        public TResult Case<TResult>(Func<TValue, TResult> success, Func<TResult> failure)
-        {
-            return _isSuccess ? success(_value) : failure();
-        }
-
-        [Pure]
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return (EqualityComparer<TValue>.Default.GetHashCode(_value) * 397) ^ _isSuccess.GetHashCode();
-            }
-        }
-
-        [Pure]
-        public override string ToString()
-        {
-            return _isSuccess ? $"Success:{_value}" : "Failure";
         }
     }
 }
