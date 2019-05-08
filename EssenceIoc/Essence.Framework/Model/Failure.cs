c2b6ad02b5e69711a2141b3bd637f9ec016b711a
Tomáš Kratěna <36986324+tkratena@users.noreply.github.com>
2019-05-08 13:25:09 +0200

    Equatability of Result, Success and Failure.

--- EssenceIoc/Essence.Framework/Model/Failure.cs
+++ EssenceIoc/Essence.Framework/Model/Failure.cs
@@ -1,12 +1,40 @@
+using System;
+using System.Collections.Generic;
+
 namespace Essence.Framework.Model
 {
-    public struct Failure<TError>
+    public struct Failure<TError> : IEquatable<Failure<TError>>
     {
         public TError Error { get; }
 
         internal Failure(TError error)
         {
             Error = error;
+        }
+        
+        public override bool Equals(object obj)
+        {
+            switch (obj)
+            {
+                case Failure<TError> other: 
+                    return Equals(other);
+                
+                case IEquatable<Failure<TError>> other: 
+                    return other.Equals(this);
+                
+                default:
+                    return false;
+            }
+        }
+
+        public bool Equals(Failure<TError> other)
+        {
+            return EqualityComparer<TError>.Default.Equals(Error, other.Error);
+        }
+
+        public override int GetHashCode()
+        {
+            return ((Result<object, TError>) this).GetHashCode();
         }
     }
 }
--- EssenceIoc/Essence.Framework/Model/Success.cs
+++ EssenceIoc/Essence.Framework/Model/Success.cs
@@ -1,12 +1,40 @@
+using System;
+using System.Collections.Generic;
+
 namespace Essence.Framework.Model
 {
-    public struct Success<TValue>
+    public struct Success<TValue> : IEquatable<Success<TValue>>
     {
         public TValue Value { get; }
 
         internal Success(TValue value)
         {
             Value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            switch (obj)
+            {
+                case Success<TValue> other: 
+                    return Equals(other);
+                
+                case IEquatable<Success<TValue>> other: 
+                    return other.Equals(this);
+                
+                default:
+                    return false;
+            }
+        }
+
+        public bool Equals(Success<TValue> other)
+        {
+            return EqualityComparer<TValue>.Default.Equals(Value, other.Value);
+        }
+
+        public override int GetHashCode()
+        {
+            return ((Result<TValue, object>) this).GetHashCode();
         }
     }
 }
