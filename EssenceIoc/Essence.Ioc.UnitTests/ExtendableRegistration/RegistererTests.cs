18ab6c9838dd604cd5fc8087676bcd4e97a08ff7
Tomáš Kratěna <36986324+tkratena@users.noreply.github.com>
2020-01-13 22:08:19 +0100

    Extendable registration tests

---
+++ EssenceIoc/Essence.Ioc.UnitTests/ExtendableRegistration/RegistererTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Essence.Ioc.ExtendableRegistration
+{
+    [TestFixture]
+    public class RegistererTests
+    {
+        [Test]
+        public void AddRegistrationPassesRegistrationToRegisterer()
+        {
+            var spyRegisterer = new SpyRegisterer();
+            var registrationInstance = new DummyRegistration();
+            
+            Registerer.AddRegistration(spyRegisterer, registrationInstance);
+            
+            Assert.That(spyRegisterer.AddedRegistrations, Is.EqualTo(new[] { registrationInstance }));
+        }
+
+        private class SpyRegisterer : Registerer 
+        {
+            private readonly List<IRegistration> _addedRegistrations = new List<IRegistration>();
+
+            public IReadOnlyList<IRegistration> AddedRegistrations => _addedRegistrations;
+            
+            protected override void AddRegistration(IRegistration r) => _addedRegistrations.Add(r);
+        }
+
+        private class DummyRegistration : IRegistration
+        {
+            public void Register(IRegisterer registerer)
+            {
+            }
+        }
+    }
+}
